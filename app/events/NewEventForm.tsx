2551b302fd64296246e1a053a332642024ca6e2c
KeinoC <keino.chichester@gmail.com>
2023-05-29 23:18:39 -0400

    new event form v1 complete. pending firestore post

--- app/events/NewEventForm.tsx
+++ app/events/NewEventForm.tsx
@@ -1,6 +1,8 @@
 "use client";
 import React, { useContext } from "react";
 import { EventContext } from "../Context/EventProvider/EventContext";
+import { TextInput, Checkbox, Button, Group, Box, Select } from '@mantine/core';
+import { useForm } from '@mantine/form';
 
 
 
@@ -11,31 +13,46 @@ export default function NewEventForm() {
   const { newEvent, setNewEvent } = useContext(EventContext);
   console.log(newEvent ? newEvent : "testing")
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const inputValue = e.target.value;
-    const inputName = e.target.name;
+const handleInputChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+  const inputValue = e.target.value;
+  const inputName = e.target.name;
 
-    setNewEvent((prevEvent: Event) => ({
-      ...prevEvent,
-      [inputName]: inputValue,
-    }));
-  };
+  setNewEvent((prevEvent: Event) => ({
+    ...prevEvent,
+    [inputName]: inputValue,
+  }));
+};
+
+const handleSelectionChange = (value: "string") => {
+  setNewEvent((prevEvent: Event) => ({
+    ...prevEvent, ["bookingStatus"] : value
+  }))
+};
 
   return (
+    <Box maw={300} mx="auto">
     <form>
-      <select>
-        <option value="Inquiry">Inquiry</option>
-        <option value="Pending">Pending</option>
-        <option value="Contract">Contract</option>
-      </select>
-      <input
+
+      <Select
+      onChange={handleSelectionChange}
+      value={newEvent ? newEvent.bookingStatus : ""}
+      name="bookingStatus"
+      placeholder="Enter booking status"
+      data={[
+        {value: "Inquiry", label: "Inquiry" },
+        {value: "Pending", label: "Pending" },
+        {value: "Contract", label: "Contract" }
+]}
+/>
+
+      <TextInput
         name="eventHost"
         value={newEvent ? newEvent.eventHost : ""}
         type="text"
         placeholder="Host's full name"
         onChange={handleInputChange}
       />
-      <input
+      <TextInput
         name="eventDate"
         value={newEvent.eventDate ? newEvent.eventDate : ""}
         type="date"
@@ -43,5 +60,9 @@ export default function NewEventForm() {
         onChange={handleInputChange}
       />
     </form>
+    <Group position="right" mt="md">
+          <Button type="submit">Submit</Button>
+        </Group>
+    </Box>
   );
 }
