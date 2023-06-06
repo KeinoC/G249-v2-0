b0c7fc4fb899270639fbd632dc3e546ab29bf94e
KeinoC <keino.chichester@gmail.com>
2023-06-05 22:28:29 -0400

    home page updates

---
+++ app/home/HomeComponent.tsx
@@ -0,0 +1,18 @@
+"use client";
+import React, {useContext, useState} from 'react';
+import "./HomeComponent.css"
+
+
+export default function HomeComponent() {
+
+
+    return (
+        <div className="home-page">
+    <h1 className="garden">Garden 249</h1>
+<div className="button-container">
+<button className="big-button"> Explore </button>
+<button className="big-button"> Book Now </button>
+</div>
+        </div>
+    )
+}
