5ca7a51d947c987366c03ef72d1b12e6d97b7fcd
KeinoC <keino.chichester@gmail.com>
2023-06-05 23:32:36 -0400

    home page updates again

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,22 +1,20 @@
 "use client";
-import React, {useContext, useState} from 'react';
-import NewEventForm from "./NewEventForm"
-import {EventContext} from "../Context/EventProvider/EventContext"
-import NavBar from '../home/NavBar';
+import React, { useContext, useState } from "react";
+import NewEventForm from "./NewEventForm";
+import { EventContext } from "../Context/EventProvider/EventContext";
+import NavBar from "../home/NavBar";
 
 export default function Events() {
-
-
     // Imports States along with types from state manager (AppProvider)
-    const EventListing = useContext(EventContext)
-    const {allEvents} = EventListing || {};
+    const EventListing = useContext(EventContext);
+    const { allEvents } = EventListing || {};
 
-    console.log(allEvents)
+    console.log(allEvents);
 
     return (
         <div>
-<NavBar />
-        <NewEventForm />
+            <NavBar />
+            <NewEventForm />
         </div>
-    )
+    );
 }
--- app/home/NavBar.tsx
+++ app/home/NavBar.tsx
@@ -16,4 +16,3 @@ export default function NavBar() {
         </div>
     );
 }
-document.querySelector("body")
