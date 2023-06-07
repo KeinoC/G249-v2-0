9b296822f8bdb53255d16c6bd004768e11955477
KeinoC <keino.chichester@gmail.com>
2023-06-06 23:51:16 -0400

    event calendar setup

--- app/dashboard/MyEvents/EventCalendar.tsx
+++ app/dashboard/MyEvents/EventCalendar.tsx
@@ -6,9 +6,8 @@ import { MiscContext } from "../../Context/MiscProvider/MiscContext"
 import NavBar from "../../home/NavBar"
 import MobileClientSideBar from "../MobileClientSideBar"
 import MobileTabbedDashboard from "../MobileTabbedDashboard"
-import EventCalendar from "./EventCalendar"
 
-export default function ClientEvents() {
+export default function EventCalendar() {
     // Imports States along with types from state manager (AppProvider)
     const EventListing = useContext(EventContext);
     const { allEvents } = EventListing || {};
@@ -21,8 +20,7 @@ export default function ClientEvents() {
 
     return (
         <div className="tab-display-container">
-            Hello from Client Events!
-            <EventCalendar />
+            Hello from Calendar!
         </div>
     );
 }
