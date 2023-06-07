b5a0a213abaabba5d0e24d746f78c3a22f7431c1
KeinoC <keino.chichester@gmail.com>
2023-06-07 00:35:57 -0400

    calendar bones setup

--- app/dashboard/MyEvents/EventCalendar.tsx
+++ app/dashboard/MyEvents/EventCalendar.tsx
@@ -1,28 +1,28 @@
 "use client";
 import React, { useContext, useState } from "react";
-import ClientSideBar from "../ClientSideBar"
+import ClientSideBar from "../ClientSideBar";
 import { EventContext } from "../../Context/EventProvider/EventContext";
-import { MiscContext } from "../../Context/MiscProvider/MiscContext"
-import NavBar from "../../home/NavBar"
-import MobileClientSideBar from "../MobileClientSideBar"
-import MobileTabbedDashboard from "../MobileTabbedDashboard"
-import { Group } from '@mantine/core';
-import { Calendar } from '@mantine/dates';
+import { MiscContext } from "../../Context/MiscProvider/MiscContext";
+import NavBar from "../../home/NavBar";
+import MobileClientSideBar from "../MobileClientSideBar";
+import MobileTabbedDashboard from "../MobileTabbedDashboard";
+import { Group } from "@mantine/core";
+import { Calendar } from "@mantine/dates";
+import "./EventCalendar.css"
 
 export default function EventCalendar() {
     // Imports States along with types from state manager (AppProvider)
     const EventListing = useContext(EventContext);
     const { allEvents } = EventListing || {};
-    const { isMobile }= useContext(MiscContext);
-
-
+    const { isMobile } = useContext(MiscContext);
 
     // {/* {isMobile ? <MobileClientSideBar /> : <ClientSideBar />} */}
 
-
     return (
         <div className="tab-display-container">
-            Hello from Calendar!
+            <Group position="center">
+                <Calendar />
+            </Group>
         </div>
     );
 }
